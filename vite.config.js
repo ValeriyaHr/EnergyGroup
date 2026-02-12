686810da314e619378fa4c5cb226fa5bf8af485a
Hrybok Alex <zagadum@ukr.net>
2026-02-12 16:03:20 +0200

    refactor

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import handlebars from 'vite-plugin-handlebars'
 import path from 'path'
 
 export default defineConfig({
+    base: '/EnergyGroup/',
     plugins: [
         handlebars({
             partialDirectory: path.resolve(__dirname, 'components'),
